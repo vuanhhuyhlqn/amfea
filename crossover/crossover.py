689597ffc9c9f45e7c5f17f7c91464cb57a4c98e
madmoon <madmoontokyo@gmail.com>
2025-04-22 01:41:37 +0700

    arithmetic crossover

--- crossover/crossover.py
+++ crossover/crossover.py
@@ -1,13 +1,26 @@
 import numpy as np
-from numba import jit
 
-class Crossover:
+class AbstractCrossover:
 	def __init__(self):
 		pass
 	def __call__(self, rmp, p1, p2):
 		pass
 
-class ArithmeticCrossover(Crossover):
-    def __init__(self):
-        pass
-    def crossover(self, rmp, p1, p2):
+class ArithmeticCrossover(AbstractCrossover):
+	def __init__(self):
+		pass
+
+	def crossover(self, rmp, p1, p2):
+		assert(len(rmp) == len(p1) and len(rmp) == len(p2))
+		rnd = np.random.uniform(size = rmp.shape)
+		crossover_indices = np.where(rnd < rmp)[0]
+		# print(rnd)
+		# print(rmp)
+		alpha = np.random.uniform(size=(len(crossover_indices), p1.shape[1]))
+		# print(alpha)
+		off = p1[crossover_indices] * alpha + p2[crossover_indices] * (1 - alpha)
+		return off
+
+
+	def __call__(self, rmp, p1, p2):
+		return self.crossover(rmp, p1, p2)
