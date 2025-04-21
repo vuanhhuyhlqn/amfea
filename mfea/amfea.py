910d450ae52d407c3c9491b96bec9b50f84c5e69
madmoon <madmoontokyo@gmail.com>
2025-04-22 06:39:48 +0700

    full evolve

--- mfea/amfea.py
+++ mfea/amfea.py
@@ -31,13 +31,22 @@ class AMFEA:
             self.skill_factor[i] = i % self.num_tasks
 
         self.fitness = np.zeros(self.pop_size)
+        self.best_fitness = np.zeros(self.num_tasks)
+        self.mean_fitness = np.zeros(self.num_tasks)
 
-        for task_id in range(len(tasks)):
+        for task_id in range(self.num_tasks):
             task_mask = self.skill_factor == task_id
             self.fitness[task_mask] = tasks[task_id].fitness(self.pop[task_mask])
+            self.best_fitness[task_id] = np.min(self.fitness[task_mask])
+            self.mean_fitness[task_id] = np.mean(self.fitness[task_mask])
 
-    def evolve(self):
-        num_pair = np.random.randint(0, self.pop_size)
+        print("Initialization:")
+        for task_id in range(self.num_tasks):
+            print("Task {0}:".format(task_id))
+            print("Best Fitness: {0}".format(self.best_fitness[task_id]))
+            print("Mean Fitness: {0}\n".format(self.mean_fitness[task_id]))
+
+    def get_random_parents(self, num_pair):
         p1_indices = np.random.randint(0, self.pop_size, size=num_pair)
         p2_indices = np.random.randint(0, self.pop_size, size=num_pair)
         
@@ -46,8 +55,71 @@ class AMFEA:
         
         p1_fitness = self.fitness[p1_indices]
         p2_fitness = self.fitness[p2_indices]
+        return self.pop[p1_indices], self.pop[p2_indices], p1_skill_factor, p2_skill_factor, p1_fitness, p2_fitness
+
+    def get_random_individuals(self, num):
+        p_indices = np.random.randint(0, self.pop_size, num)
+        p = self.pop[p_indices]
+        p_skill_factor = self.skill_factor[p_indices]
+        p_fitness = self.fitness[p_indices]
+        return p, p_skill_factor, p_fitness
+
+    def evolve(self):
+        num_pair = np.random.randint(0, int(self.pop_size / 2))
+        p1, p2, p1_skill_factor, p2_skill_factor, p1_fitness, p2_fitness = self.get_random_parents(num_pair)
+        #Adaptive RMP
+        armp = self.rmp(p1, p2, p1_skill_factor, p2_skill_factor, p1_fitness, p2_fitness)
+        
+        #Crossover
+        off, off_skill_factor = self.crossover(armp, p1, p2, p1_skill_factor, p2_skill_factor)
+        off_fitness = np.zeros(len(off), dtype=np.float32)
+
+        #Calculate crossover offsprings fitness
+        for task_id in range(self.num_tasks):
+            task_mask = off_skill_factor == task_id 
+            off_fitness[task_mask] = self.tasks[task_id].fitness(off[task_mask])
+
+        #Mutation
+        num_mutation = np.random.randint(0, int(self.pop_size) / 2)
+        off_mut, off_mut_skill_factor, off_mut_fitness = self.get_random_individuals(num_mutation)
+        off_mut = self.mutation(off_mut)
+        
+        #Calculate mutation offsprings fitness
+        for task_id in range(self.num_tasks):
+            task_mask = off_mut_skill_factor == task_id 
+            off_mut_fitness[task_mask] = self.tasks[task_id].fitness(off_mut[task_mask])
+        
+        ipop = np.concatenate([self.pop, off, off_mut])
+        iskill_factor = np.concatenate([self.skill_factor, off_skill_factor, off_mut_skill_factor])
+        ifitness = np.concatenate([self.fitness, off_fitness, off_mut_fitness])
+
+        self.pop = np.array([]).reshape(0, self.indi_len)
+        self.fitness = []
+        self.skill_factor = []
+
+        for task_id in range(self.num_tasks):
+            survive_size = int(self.pop_size / self.num_tasks)
+            task_mask = iskill_factor == task_id
+            
+            tpop = ipop[task_mask]
+            tfitness = ifitness[task_mask]
+            assert(len(tpop) == len(tfitness))
+            
+            self.best_fitness[task_id] = np.min(tfitness[survive_indices])
+            self.mean_fitness[task_id] = np.mean(tfitness[survive_indices])
+
+            survive_indices = np.argpartition(tfitness, survive_size - 1)[:survive_size]
+            self.pop = np.concatenate([self.pop, tpop[survive_indices]])
+            self.fitness = np.concatenate([self.fitness, tfitness[survive_indices]])
+            self.skill_factor = np.concatenate([self.skill_factor, np.full(survive_size, task_id)])
 
 
-    def fit(self, num_gen, llm_rate):
+    def fit(self, num_gen, llm_rate=None):
         for gen in range(num_gen):
-            pass
+            self.evolve()
+            print("Gen {0}".format(gen))
+            for task_id in range(self.num_tasks):
+                print("Task {0}:".format(task_id))
+                print("Best Fitness: {0}".format(self.best_fitness[task_id]))
+                print("Mean Fitness: {0}\n".format(self.mean_fitness[task_id]))
+            
--- rmp/NormalRMP.py
+++ rmp/NormalRMP.py
@@ -6,5 +6,5 @@ class NormalRMP(AbstractRMP):
         pass
     def get_rmp(self, size):
         return np.full(size, 0.3)
-    def __call__(self, size):
-        return self.get_rmp(size)
+    def __call__(self, p1, p2, p1_skill_factor, p2_skill_factor, p1_fitness, p2_fitness):
+        return self.get_rmp(len(p1))
