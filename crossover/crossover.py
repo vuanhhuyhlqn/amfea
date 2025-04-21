000a35795bf87fbb44d2863045d0c631a7581340
madmoon <madmoontokyo@gmail.com>
2025-04-22 00:25:28 +0700

    2nd commit

---
+++ crossover/crossover.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numba import jit
+
+class Crossover:
+	def __init__(self):
+		pass
+	def __call__(self, rmp, p1, p2):
+		pass
+
+class ArithmeticCrossover(Crossover):
+    def __init__(self):
+        pass
+    def crossover(self, rmp, p1, p2):
