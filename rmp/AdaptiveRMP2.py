5231d04aca910733821072456f51a3e7d649d248
madmoon <madmoontokyo@gmail.com>
2025-04-23 12:58:30 +0700

    cec17 task

--- rmp/AdaptiveRMP2.py
+++ rmp/AdaptiveRMP2.py
@@ -1,5 +1,30 @@
 import numpy as np
 from .AbstractRMP import AbstractRMP
+from llm import *
+from crossover import *
+import os
+
+class Population:
+    def __init__(self, pop_size, num_gen):
+        self.pop_size = pop_size
+        self.num_gen = num_gen
+        self.pop = []
+        
+        DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+        self.model = DeepsekModel2(DEEPSEEK_API_KEY)
+
+        for _ in range(self.pop_size):
+            self.pop.append(self.model.init_idea_from_llm())
+        
+        for p in self.pop:
+            print(p)
+    
+    def evaluate(self, p, p1, p2, p1_skill_factor, p2_skill_factor, p1_fitness, p2_fitness):
+        rmp_function = self.model.gen_code_from_idea(p)
+        f = {}
+        exec(rmp_function, f)
+        rmp = f["get_rmp"](p1, p2, p1_skill_factor, p2_skill_factor)
+        print(rmp)
 
 class ApdaptiveRMP2(AbstractRMP):
     def __init__(self):
--- rmp/__init__.py
+++ rmp/__init__.py
@@ -1,3 +1,4 @@
 from .AbstractRMP import AbstractRMP
 from .NormalRMP import NormalRMP
 from .AdaptiveRMP import AdaptiveRMP
+from .AdaptiveRMP2 import AdaptiveRMP2, Population
