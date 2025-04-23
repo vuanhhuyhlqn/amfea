9e0192cf9924d7e8c7cd040934c023b08b2aa86a
madmoon <madmoontokyo@gmail.com>
2025-04-23 12:13:04 +0700

    more and more

---
+++ llm/DeepseekModel2.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+import re
+init_idea_text = open("llm/prompts2/init_idea.txt", "r").read()
+gen_function_text = open("llm/prompts2/gen_function.txt", "r").read()
+def split_sentences(text):
+    lines = text.strip().split('\n')
+    sentences = [re.sub(r'^\d+\.\s*', '', line).strip() for line in lines]
+    return sentences
+def remove_first_last_lines(text):
+    lines = text.strip().split('\n')
+    result = lines[1:-1]
+    return '\n'.join(result)
+class DeepsekModel2:
+    def __init__(self, API_KEY):
+        self.API_KEY = API_KEY
+
+    def init_idea_from_llm(self):
+        client = OpenAI(api_key=self.API_KEY, base_url="https://api.deepseek.com")
+
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {"role": "user", "content": init_idea_text},
+            ],
+            stream=False
+        )
+        return split_sentences(response.choices[0].message.content)
+
+    def gen_code_from_idea(self, ideas):
+        ideas_text = ""
+        for idea in ideas:
+            ideas_text += idea + "\n"
+        
+        u = gen_function_text.format(ideas)
+
+        client = OpenAI(api_key=self.API_KEY, base_url="https://api.deepseek.com")
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {"role": "user", "content": u},
+            ],
+            stream=False
+        )
+        return remove_first_last_lines(response.choices[0].message.content)
---
+++ rmp/AdaptiveRMP2.py
@@ -0,0 +1,8 @@
+import numpy as np
+from .AbstractRMP import AbstractRMP
+
+class ApdaptiveRMP2(AbstractRMP):
+    def __init__(self):
+        pass
+    def __call__(self, p1, p2, p1_skill_factor, p2_skill_factor):
+        pass
