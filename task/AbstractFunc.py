fix shift rotation decode dimension mismatch

task/AbstractFunc.py
@@ -14,9 +14,9 @@ class AbstractFunc:
         else:
             self.shift = np.zeros(dim)
     def shift_rotation_decode(self, x):
-        print(x.shape)
-        print(self.shift.shape)
-        print(self.rotation_matrix.shape)
-        return self.rotation_matrix @ (x - self.shift)
+        # print(x.shape)
+        # print(self.shift.shape)
+        # print(self.rotation_matrix.shape)
+        return self.rotation_matrix @ (x - self.shift.flatten())
     def __call__(self, x):
         pass