d2a3f7d562f47bbd0f6d10ea2b3b6935498c5b54
madmoon <madmoontokyo@gmail.com>
2025-05-03 23:22:21 +0700

    tsp funny

---
+++ task/TSP.py
@@ -0,0 +1,20 @@
+from .AbstractTask import AbstractTask
+import tsplib
+import numpy as np
+
+class TSPTask(AbstractTask):
+    def __init__(self, problem_name):
+        prob = tsplib.Problem('tsp', problem_name)
+        self.num_cities = prob.problem_size
+        self.dist_mat = prob.distance_matrix
+
+    def decode(self, p):
+        decoded_p = np.argsort(p) + 1
+        return decoded_p
+
+    def fitness(self, p):
+        dp = self.decode(p)
+        distances = np.sum(self.dist_mat[dp[:, 1:], dp[:, :-1]], axis=1)
+        np.add(distances, self.dist_mat[0, dp[:, 0]], out=distances)
+        np.add(distances, self.dist_mat[dp[:, -1], 0], out=distances)
+        return distances
--- task/__init__.py
+++ task/__init__.py
@@ -3,3 +3,4 @@ from .DummyTask import DummyTask
 from .GNBGTask import GNBGTask
 from .CEC17Func import *
 from .CEC17Task import *
+from .TSP import *
